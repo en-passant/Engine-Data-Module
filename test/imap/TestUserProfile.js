614f6562675da23c63e03e29fb218d00a9721af7
Parag <parag@example.com>
2014-02-13 00:54:00 +0530

    Added mocha test setup with first basic test for imap

---
+++ test/imap/TestUserProfile.js
@@ -0,0 +1,32 @@
+process.env.NODE_ENV = 'test';
+var EDM = require( 'engine-data-module' );
+var tokenStore = new EDM.DummyTokenStore();
+var assert = require('assert');
+var datamodule = new EDM.DataModule({
+  tokenStore: tokenStore,
+	services: [ 'imap' ]
+});
+
+describe('Testing Imap User Profile', function() {
+  before(function() {
+    tokenStore.storeUserTokens(
+      'curtis', 'acct:imap:curtislacy221@yahoo.com@imap.mail.yahoo.com',
+	    {
+		     username: 'curtislacy221@yahoo.com',
+		     password: 'enR9GBEU-Zrqm-2xev_PpQ_cuN-TqyTg',
+		     host: 'imap.mail.yahoo.com',
+		     port: '993',
+		     secure: 'on'
+	    }, function(){});
+  });
+
+  it('should show contact a form', function() {
+    datamodule.fetcher.fetch(
+      'ldengine://curtis//@acct:imap:curtislacy221@yahoo.com@imap.mail.yahoo.com/user/curtis.lacy',
+       function(error, result) {
+         assert.equal(error, null);
+         //done();
+       });
+  });
+
+});
