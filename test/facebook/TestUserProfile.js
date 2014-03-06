1834e7876a1d6df30e453735b54a972792b80830
Parag <parag@example.com>
2014-03-06 21:53:58 +0530

    Facebook auth is not working

--- test/facebook/TestUserProfile.js
+++ test/facebook/TestUserProfile.js
@@ -1,4 +1,6 @@
 process.env.NODE_ENY = 'test';
+process.env.FACEBOOK_KEY = '500475259980837';
+process.env.FACEBOOK_SECRET = '1502a48345e37d6555167fd2c62ad83';
 var EDM = require('engine-data-module');
 var fs = require('fs');
 var tokenStore = new EDM.DummyTokenStore();
@@ -10,12 +12,12 @@ var datamodule = new EDM.DataModule({
 
 describe('Testing Facebook User Profile', function(){
   before(function(){
-    tokenStore.storeUserTokens(
-     'Yuho','acct:facebook:100007891360262',
+    tokenStore.storeApplicationTokens('facebook',
      {
-       username: 'yuho.jeta',
-       password: 'Datamodule1234',
-       secure:'on'
+       'clientID': '500475259980837',
+       'clientSecret':'1502a48345e37d6555167fd2c62ad837',
+       'callbackURL' :'/',
+       'callbackPath' : '/'
      }, function(){
      });
   });
@@ -23,9 +25,10 @@ describe('Testing Facebook User Profile', function(){
   it('should return users facebook profile', function() {
     datamodule.fetcher.fetch('ldengine://gmail:110555463901248997040//@acct:facebook:100007891360262/user/100007891360262',
      function(error, result) {
-       console.log(result);
-       console.log(error);
+       console.log("this is result "+result);
+       console.log("this is error "+error);
        assert.equal(error, null);
+       assert.equal(result.username,'yuho.tata');
      });
   });
 
